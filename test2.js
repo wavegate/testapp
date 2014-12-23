317b266ca8c4318386ebcad9289d915859268978
Ying Wang <yingw787@gmail.com>
2014-12-23 12:51:18 -0500

    I can send REST interface calls

--- test2.js
+++ test2.js
@@ -1,17 +1,14 @@
 var http = require('http');
 
 var options = {
-  hostname: '73.161.74.206',
+  hostname: 'colab-sbx-138.oit.duke.edu',
   port: 5000,
   path: '/api/connection',
-  method: 'GET'
+  method: 'GET',
   headers: {
-      Content-Type: application/json, 
+      "Content-Type": "application/json", 
+      "X-Api-Key": "4FE8C4FCC1964E7F9820142E519C18DB"
   }
-  X-Api-Key: '1C7A2DC310AE4BCB977F0C7413F32359',
-  // headers: {
-  //   Content-Type: application/json
-  // }
 };
 
 var req = http.request(options, function(res) {
